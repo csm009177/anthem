b4cf14a7d35eeb1d4264c7825ad64e2eef0acb8f
csm <csm009177@gmail.com>
2024-01-25 11:50:20 +0900

    make  layout structure

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { ChildrenProps } from "./ChildrenProps";
+
+
+// Customer page를 렌더링하는 파일입니다.
+
+
+export default function Main() {
+  return (
+    <div className='flex justify-center items-center flex-col w-screen h-screen overflow-hidden'>
+    </div>
+  );
+}
