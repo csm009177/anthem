00fb3212c30e7d2ddc17d32975e045f16d028685
csm <csm009177@gmail.com>
2024-01-26 22:01:51 +0900

    칸을 가사 수만큼 나누기

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -6,10 +6,14 @@ import { useRouter } from "next/navigation";
 
 // 로그인 컴포넌트 정의
 export default function Login() {
+  // 애국가 문장 정의
+  const anthemText =
+    "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세";
+
   // 상태 변수 초기화
-  const [lyrics, setLyrics] = useState("");
+  const [lyrics, setLyrics] = useState(Array(anthemText.length).fill(""));
   const [message, setMessage] = useState("");
-  
+
   // Next.js의 라우터 훅 사용
   const router = useRouter();
 
@@ -25,18 +29,16 @@ export default function Login() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ lyrics }),
+        body: JSON.stringify({ lyrics: lyrics.join("") }),
       });
 
       // HTTP 응답이 OK인 경우
       if (response.ok) {
         // 로그인 성공 시 /korean 페이지로 이동
         router.push("/korean");
-        console.log(lyrics);
         setMessage("당신은 애국자입니다");
       } else {
         // 로그인 실패 시
-        console.log(lyrics);
         setMessage("당신은 매국노입니다");
       }
     } catch (error) {
@@ -48,20 +50,26 @@ export default function Login() {
 
   // 로그인 컴포넌트 JSX 반환
   return (
-    <div className="flex flex-col justify-center items-center h-lvh w-screen h-1/6 bg-green-900 text-white" >
-      
+    <div className="flex flex-col justify-center items-center h-lvh bg-green-900">
       <form
-        className="h-32 flex flex-col items-end justify-around"
+        className="h-32 flex flex-row flex-wrap items-end justify-around p-40"
         onSubmit={handleLogin}
       >
-        {/* 입력 필드 */}
-        <input
-          className="border border-black"
-          type="text"
-          value={lyrics}
-          placeholder="type first verse"
-          onChange={(e) => setLyrics(e.target.value)}
-        />
+        {/* 각 글자에 대한 입력 필드 생성 */}
+        {anthemText.split("").map((char, index) => (
+          <input
+            key={index}
+            className="w-fit border border-black"
+            type="text"
+            value={lyrics[index]}
+            placeholder={`_`}
+            onChange={(e) => {
+              const updatedLyrics = [...lyrics];
+              updatedLyrics[index] = e.target.value;
+              setLyrics(updatedLyrics);
+            }}
+          />
+        ))}
         {/* 로그인 버튼 */}
         <button className="border border-black" type="submit">
           검사
