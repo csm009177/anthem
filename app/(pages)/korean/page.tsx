90d8426a3b29fe8a379f512899033f30e05d94b5
csm <csm009177@gmail.com>
2024-01-27 11:09:36 +0900

    코리아 페이지에 불법 접근하는 매국노 잡아내기

--- app/(pages)/korean/page.tsx
+++ app/(pages)/korean/page.tsx
@@ -1,7 +1,16 @@
 'use client'
-
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
 
 export default function Korean() {
+  const Token = localStorage.getItem("Token");
+  const router = useRouter();
+  
+  if (!Token) {
+    router.push("/");
+    alert("꼼수 부리지 마라. 더러운 매국노야");
+  }
+
   return (
     <div className='flex justify-center items-center flex-col w-screen h-screen bg-green-900 text-white p-auto'>
       Koean
