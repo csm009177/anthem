a96875f7152958336144532f9410dfb70135f911
csm <csm009177@gmail.com>
2024-01-28 01:22:19 +0900

    info 페이지 추가 및

--- app/(pages)/info/page.tsx
+++ app/(pages)/info/page.tsx
@@ -1,12 +1,12 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Button from '../../ui/button/button';
+import Button from "../../ui/button/button";
 
-export default function Korean() {
+export default function Info() {
   const Token = localStorage.getItem("Token");
   const router = useRouter();
-  
+
   if (!Token) {
     router.push("/");
     alert("꼼수 부리지 마라. 더러운 매국노야");
@@ -14,9 +14,13 @@ export default function Korean() {
 
   return (
     <div className="flex flex-col justify-center items-center h-lvh  bg-green-900 text-white p-auto">
-      <div></div>
-      <Button url='info' title='Personnel Information'/>
-      
+      <Button url="korean" title="korean" />
+      <div>
+        Personnel information
+        <div>CHOI SUNGMIN</div>
+        <div> 82+010-9558-1007</div>
+        <div>csm009177@gamil.com</div>
+      </div>
     </div>
   );
 }
