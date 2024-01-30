faf3e5a51831de2e5645ba2a1a49e6589d4cc5fc
csm <csm009177@gmail.com>
2024-01-30 10:37:03 +0900

    stack page 생성 및 서버에  토큰 발행 로직 작성

--- app.js
+++ app.js
@@ -107,6 +107,33 @@ app.prepare().then(() => {
     });
   });
 
+  // passThree
+  server.post("/passThree", (req, res) => {
+    const { answer } = req.body;
+
+    // 해당 사용자가 존재하는지 확인하는 쿼리
+    const query = "SELECT * FROM pass WHERE answer= ? AND num = 3";
+    connection.query(query, [answer], (err, results, fields) => {
+      if (err) {
+        console.error("Error logging in:", err);
+        res.status(500).json({ message: "로그인에 실패했습니다." });
+        return;
+      }
+
+      // 로그인 성공 여부 확인
+      if (results.length > 0) {
+        const user = results[0];
+        const tokenPayload = {
+          answer: user.answer,
+        };
+        const tokenThree = jwt.sign(tokenPayload, secretKey, { expiresIn: '1h' });
+        res.status(200).json({ message: "당신은 html을 잘 압니다", tokenThree });
+      } else {
+        res.status(401).json({ message: "당신은 html을 모릅니다" });
+      }
+    });
+  });
+
   // Next.js 서버에 라우팅 위임
   server.all('*', (req, res) => {
     return handle(req, res);
---
+++ app/(pages)/stack/page.tsx
@@ -0,0 +1,83 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import Button from '../../ui/button/button';
+
+export default function Stack() {
+  const token = localStorage.getItem("token");
+  const tokenOne = localStorage.getItem("tokenOne");
+  const tokenTwo = localStorage.getItem("tokenTwo");
+  const [answer, setLyrics] = useState("");
+  const [message, setMessage] = useState("");
+  const router = useRouter();
+  
+  if (!token) {
+    router.back();
+    alert("꼼수 부리지 마라. 더러운 매국노야");
+  }
+  if (!tokenOne) {
+    router.back();
+    alert("꼼수 부리지 마라. 더러운 얌생이야");
+  }
+  if (!tokenTwo) {
+    router.back();
+    alert("꼼수 부리지 마라. 더러운 얌생2야");
+  }
+
+  const handlePass = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/passThree", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ answer }),
+      });
+      if (response.ok) {
+        // 토큰 발행을 위한 서버 응답 기다리기
+        const data = await response.json();
+        const tokenThree = data.tokenThree;
+  
+        // 토큰을 안전하게 저장
+        localStorage.setItem("tokenThree", tokenThree);
+        router.push("/other");
+        setMessage("정답입니다");
+        console.log(`input answer : ${answer}` )
+      } else {
+        setMessage("틀렸습니다");
+        console.log(`input answer : ${answer}` )
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("서버 오류");
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-lvh  bg-green-900 text-white p-auto">
+      <div>
+        <Button url='Javascript' title='Javascript Useage'/>
+      </div>
+      <div className='flex flex-col h-full justify-center items-center'>
+      <form
+        className="h-32 flex items-end justify-around"
+        onSubmit={handlePass}
+      >
+        <input
+          className="border border-black text-black"
+          type="text"
+          value={answer}
+          placeholder="type first verse"
+          onChange={(e) => setLyrics(e.target.value)}
+        />
+        <button className="border border-black" type="submit">
+          검사
+        </button>
+      </form>
+      {message && <p>{message}</p>}
+      </div>
+    </div>
+  );
+}
