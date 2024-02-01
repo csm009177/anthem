3cdf7d38c28cdf5ec2c515b1318a7405e5a700c2
csm <csm009177@gmail.com>
2024-02-01 17:14:36 +0900

    stack , stackOne, stackTwo 페이지 헤더와 푸터 위치 조정

--- app/(pages)/stackOne/page.tsx
+++ app/(pages)/stackOne/page.tsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
 import Button from '../../ui/button/button';
+import Header from '../../ui/header';
+import Foot from '../../ui/footer';
 
 export default function StackOne() {
   const token = localStorage.getItem("token");
@@ -61,15 +63,16 @@ export default function StackOne() {
   };
 
   return (
-    <div className="flex flex-col h-lvh  bg-green-900 text-white p-auto">
+    <div className="flex flex-col h-screen w-screen bg-green-900 text-white p-auto">
+      <Header/>
       <div>
-        <Button url='react' title='React Useage'/>
+        <Button url='info' title='Personnel Information'/>
       </div>
       <div className='flex flex-col h-full justify-center items-center'>
       <form
-        className="h-32 flex items-end justify-around"
+        className=" flex justify-around"
         onSubmit={handlePass}
-      >
+      >{`<Script>`}
         <input
           className="border border-black text-black"
           type="text"
@@ -77,12 +80,14 @@ export default function StackOne() {
           placeholder="type first verse"
           onChange={(e) => setLyrics(e.target.value)}
         />
+        {`<Script/>`}
         <button className="border border-black" type="submit">
           검사
         </button>
       </form>
       {message && <p>{message}</p>}
       </div>
+      <Foot/>
     </div>
   );
 }
